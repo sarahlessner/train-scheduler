a398960b574855e0e32bf62e97a3259f630f0620
Sarah Lessner <sarahlessner@Sarahs-MacBook-Pro.local>
2017-06-09 23:11:08 -0700

    yay its basically done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-// Initialize Firebase
+	// Initialize Firebase
    var config = {
     apiKey: "AIzaSyAhmGGs74CTF3OvdTAfn0VAqnDu87UHNZ4",
     authDomain: "train-scheduler-77540.firebaseapp.com",
@@ -8,107 +8,135 @@ $( document ).ready(function() {
     projectId: "train-scheduler-77540",
     storageBucket: "train-scheduler-77540.appspot.com",
     messagingSenderId: "635654226813"
-  };
-  firebase.initializeApp(config);
-// reference for yourself: firebase.database.ServerValue.TIMESTAMP
+  	};
+
+  	firebase.initializeApp(config);
+	// reference for yourself: firebase.database.ServerValue.TIMESTAMP
 
   	var database = firebase.database();
 
   	var trainName = "";
   	var destination = "";
   	var firstTrain = "";
-  	var frequency = 0;
-  	//calculated using moment.js and firstTrain/frequency user input
-  	var nextArrival = 0;
-  	var minutesAway = 0;
+  	var frequency = "";
+
+	//when #submit-button is clicked
+	$("#submit-button").on("click", function(){
+
+		//prevent default
+		event.preventDefault();
+		
+		// #train-name 
+		trainName = $("#train-name").val().trim();
+
+		// #destination 
+		destination = $("#destination").val().trim();
+
+		// #first-train 
+		firstTrain = $("#first-train").val().trim();
+
+		// #frequency
+		frequency = $("#frequency").val().trim();
+
+		//If any of the fields are empty - alert user
+		if ((trainName === "") || (destination === "") || 
+		   (firstTrain === "") || (frequency === "")) {
+			alert("Please complete all fields before submitting!");
+		}
+
+		else {
+
+		$("#train-name").val('');
+		$("#destination").val('');
+		$("#first-train").val('');
+		$("#frequency").val('');
+
+		//reference with key/value pairs for storing user input in firebase
+
+		database.ref().push({
+	        trainName: trainName,
+	        destination: destination,
+	        firstTrain: firstTrain,
+	        frequency: frequency
+	      });
+		};
+	});
+
+	//get info from firebase
+	database.ref().on("child_added", function(snapshot) {
+		//save the key as a variable
+		var myKey = snapshot.key;
+
+		//fetch user input from database
+		var trainName = snapshot.val().trainName;
+		var destination = snapshot.val().destination;
+		var frequency = snapshot.val().frequency;
+		var firstTrain = snapshot.val().firstTrain;
+
+		//initializing moment objects 
+		var mFirstTrain = moment(firstTrain, "HH:mm");
+		var currentTime = moment();
+
+		// get diff bt current time and first train
+		var timeDiff = currentTime.diff(mFirstTrain, 'minutes');
+		var minutesAway = 0;
+		var nextTrain = moment();
+		// check if time difference is less than or equal to 0 to determine if first train has not come yet
+		if (timeDiff <= 0) {
+		// set minutesaway to the time that remains before the first train
+			minutesAway = -timeDiff;
+		// set the next train to the first scheduled train since the first one hasn't arrived yet
+			nextTrain = mFirstTrain;
+
+		}
+
+		else {
+		//convert users input of frequency to an integer
+			var frequencyToInt = parseInt(frequency);
+		//find out how long since the most recent train
+			var timeSinceLast = (timeDiff % frequencyToInt);
+		//subtract that from the trains frequency to determine how many minutes until the next train
+			minutesAway = (frequencyToInt - timeSinceLast);
+			nextTrain = currentTime.add(minutesAway, 'minutes');
+
+		}
 
 
+		var myRow = $("<tr>");
 
-//when #submit-button is clicked
-$("#submit-button").on("click", function(){
+		myRow.append($("<td>").html(trainName));
+		myRow.append($("<td>").html(destination));
+		myRow.append($("<td>").html(frequency));
+		myRow.append($("<td>").html(nextTrain.format("HH:mm")));
+		myRow.append($("<td>").html(minutesAway));
+		//delete and refresh buttons
+		//delete
+		var myData = $("<td>");
+		var myBtn = $("<button>");
+		var mySpan = $("<span>");
+		myBtn.attr("class", "remove-button");
+		myBtn.attr("data-key", myKey);
+		mySpan.attr("class", "glyphicon glyphicon-remove");
+		mySpan.attr("aria-hidden", "true");
+		myBtn.append(mySpan);
+		myData.append(myBtn);
+		myRow.append(myData);
+		//another way to add the button
+		// myRow.append("<td>"+"<button id='refresh-button'>"+"<span class='glyphicon glyphicon-remove' aria-hidden='true'>"+"</span>"+"</button>"
 
-	//prevent default
-	event.preventDefault();
-	
-	// #train-name 
-	trainName = $("#train-name").val().trim();
+		//append all to id table-body
+		$("#table-body").append(myRow);
 
-	// #destination 
-	destination = $("#destination").val().trim();
+	});
 
-	// #first-train 
-	firstTrain = $("#first-train").val().trim();
+	//remove train
+	$("body").on("click", ".remove-button", function(){
+	     //removes from display
+	     $(this).closest('tr').remove();
+	     //figure out how to remove in firebase
+	     database.ref().child($(this).attr("data-key")).remove();
 
-	// #frequency
-	frequency = $("#frequency").val().trim();
-
-	//If any of the fields are empty - alert user
-	if ((trainName === "") || (destination === "") || 
-	   (firstTrain === "") || (frequency === "")) {
-		alert("Please complete all fields before submitting!");
-	}
-
-	else {
-
-	$("#train-name").val('');
-	$("#destination").val('');
-	$("#first-train").val('');
-	$("#frequency").val('');
-
-	//reference with key/value pairs for storing user input in firebase
-
-	database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency
-      });
-	};
-});
-
-
-
-
-
-database.ref().on("child_added", function(snapshot) {
-
-var trainName = snapshot.val().trainName;
-var destination = snapshot.val().destination;
-var frequency = snapshot.val().frequency;
-var firstTrain = snapshot.val().firstTrain;
-
-// var checkTime = //using moment pass in first train time
-// var currentTime = moment();
-
-// // get diff bt check and current time
-
-// var timeDiff = // another moment calculation (difference)
-
-
-var myRow = $("<tr>");
-
-
-myRow.append("<td>"+trainName+"</td>");
-myRow.append("<td>"+destination+"</td>");
-myRow.append("<td>"+frequency+"</td>");
-//replace with variable storing calculation for next arrival
-myRow.append("<td>"+frequency+"</td>");
-//replace with variable storing calculation for minutes away
-myRow.append("<td>"+frequency+"</td>");
-//delete button
-myRow.append("<td>"+"<button id='refresh-button'>"+"<span class='glyphicon glyphicon-refresh' aria-hidden='true'>"+"</span>"+"</button>"+"<button id='delete-button'>"+"<span class='glyphicon glyphicon-remove' aria-hidden='true'>"+"</span>"+"</button>"+"</td>");
-//append to id table-body
-$("#table-body").append(myRow);
-
-});
-
-//remove train
-$("body").on("click", "#delete-button", function(){
-     //removes from display
-     $(this).closest('tr').remove();
-     //figure out how to remove in firebase
-     database.remove();
-});
+	});
 
 
 
